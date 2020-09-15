e0f3b17e8b77a2eb551d9e2ca36695fd029ab71c
Jacqueline <maaaander@gmail.com>
2020-09-15 16:43:30 -0400

    fixed cron statement

--- schedule.sh
+++ schedule.sh
@@ -1,2 +1,4 @@
-fileLocation = /Users/Your/Folder/Path
-(crontab -l | grep "00 * * * * cd $fileLocation &&  python cal.py") || { crontab -l; "00 * * * * cd $fileLocation &&  python cal.py"; } | crontab
+fileLocation="/Users/Jacqueline/PycharmProjects/calendar"
+
+(crontab -l ; echo "00 * * * * cd $fileLocation &&  python cal.py") | sort - | uniq - | crontab -
+
